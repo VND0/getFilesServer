b951ae60eeaa330c181ebd2e218ab97ae7b9fcd3
varla <varlamov577123@gmail.com>
2024-01-05 11:39:45 +0300

    Возвращено использование класса Checker. Использован модуль uvicorn для запуска сервера. В классе Checker атрибуты сделаны внутренними.

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 import shutil
+import uvicorn
 
 
 class Checker:
@@ -12,9 +13,9 @@ class Checker:
             - изменение каталога.
     """
 
-    allowed_extensions = ("txt", "docx", "doc", "ppt", "pptx", "rtf", "xls", "xslx",
+    __allowed_extensions = ("txt", "docx", "doc", "ppt", "pptx", "rtf", "xls", "xslx",
                           "pdf", "jpg", "jpeg", "png", "bmp", "heic")
-    forbidden_patterns = ("../", "/..", "\b", "\\b")
+    __forbidden_patterns = ("../", "/..", "\b", "\\b")
 
     def __check_file_by_filename(self, filename: str) -> bool:
         """
@@ -26,13 +27,13 @@ class Checker:
             :return: True: проверка пройдена; False: проверка не пройдена.
         """
 
-        for pattern in self.forbidden_patterns:
+        for pattern in self.__forbidden_patterns:
             if pattern in filename:
                 return False
 
         dot_rindex = filename.rindex(".")
         extension = filename[dot_rindex + 1:].lower()
-        if extension not in self.allowed_extensions:
+        if extension not in self.__allowed_extensions:
             return False
         else:
             return True
@@ -52,15 +53,26 @@ class Checker:
 
 def main() -> None:
     app = FastAPI()
+    checker = Checker()
 
     @app.post("/upload/")
     async def upload_file(file: UploadFile = File(...)):
+        try:
+            filename = file.filename
+            allowed = checker.complex_check_file(filename)
+            if not allowed:
+                return JSONResponse(content={"message": f"It is not allowed to upload {filename}"}, status_code=403)
+        except Exception as e:
+            return JSONResponse(content={"message": f"An error occurred: {e}"}, status_code=500)
+
         try:
             with open(f"destination\\{file.filename}", "wb") as buffer:
                 shutil.copyfileobj(file.file, buffer)
             return JSONResponse(content={"message": "File uploaded successfully"})
         except Exception as e:
             return JSONResponse(content={"message": f"An error occurred: {e}"}, status_code=500)
+
+    uvicorn.run(app)  # On server you should add here host and port.
 
 
 if __name__ == '__main__':
